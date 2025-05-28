38f57ce9face1d12471c1e9438edb742ed92deb4
Victoria Florindo <florindovictoria24@gmail.com>
2025-05-28 23:33:05 +0000

    Estaría bueno que los cartelitos sean de otro color porque se confunde con el fondo

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border bg-card text-card-foreground", // Changed bg-background to bg-card and text-foreground to text-card-foreground
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
